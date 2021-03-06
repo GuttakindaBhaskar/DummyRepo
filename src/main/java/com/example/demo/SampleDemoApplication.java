6783e8f4ec6c839f55c55ca9f49e7a767f0b9345
GuttakindaBhaskar <bhaskarforreg@gmail.com>
2021-03-06 16:24:19 +0530

    WH-545-Updated Commit

--- src/main/java/com/example/demo/SampleDemoApplication.java
+++ src/main/java/com/example/demo/SampleDemoApplication.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleDemoApplication {
-
+public static Logger log=LoggerFactory.getLogger(SampleDemoApplication.class);
 	public static void main(String[] args) {
 
 		SpringApplication.run(SampleDemoApplication.class, args);
+		log.info("STARTED");
 		int a;
 		System.out.println("SampleDemoApplication.main()");
+		log.info("ENDED");
 	}
 
 }
