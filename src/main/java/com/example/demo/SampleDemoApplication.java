da5a6597a83b7544f497cea3b0720229bf0248d6
GuttakindaBhaskar <bhaskarforreg@gmail.com>
2021-03-06 16:18:06 +0530

    third commit

--- src/main/java/com/example/demo/SampleDemoApplication.java
+++ src/main/java/com/example/demo/SampleDemoApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleDemoApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(SampleDemoApplication.class, args);
-		System.out.println("SampleDemoApplication.main()");
+		int a;
 		System.out.println("SampleDemoApplication.main()");
 	}
 
